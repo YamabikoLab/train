9648b402b5f72af4ab859ef3cff5ef19078aa387
YamabikoHatake <morino.yamabiko3@gmail.com>
2021-10-18 12:59:15 +0900

    modify

--- train/runtimes/app/start-rails.sh
+++ train/runtimes/app/start-rails.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export PATH=$PATH:/home/train/.rbenv/shims
 export PATH=$PATH:/home/train/homebrew/bin
-/var/www/html/bin/rails s -p 80 -b 0.0.0.0
+bundle exec rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- /var/www/html/bin/rails s -p 80 -b 0.0.0.0
